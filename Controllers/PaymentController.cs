82229e0a5103cc70d2fcbabe4b892b1722051c7c
Marcin Bebenek <bebenekmarcin@gmail.com>
2019-10-01 14:16:49 +0100

    Added in-memory integration test for API

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Checkout.PaymentGatewayApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Checkout.PaymentGatewayApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly ILogger<PaymentController> _logger;
+
+        public PaymentController(ILogger<PaymentController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: api/Payment
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST: api/Payment
+        [HttpPost]
+        public void Post([FromBody] PaymentRequest payment)
+        {
+            Ok();
+        }
+    }
+}
---
+++ Models/PaymentRequest .cs
@@ -0,0 +1,11 @@
+﻿namespace Checkout.PaymentGatewayApi.Models
+{
+    public class PaymentRequest
+    {
+        public string CardNumber { get; set; }
+        public string ExpiryMonth { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+        public string Cvv { get; set; }
+    }
+}
